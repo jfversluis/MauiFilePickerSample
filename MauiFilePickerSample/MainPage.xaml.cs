0fc2defb5641aa2c3470b1f7b5fad26510b7b210
Gerald Versluis <gerald@verslu.is>
2022-09-27 11:15:55 +0200

    Implement FilePicker

--- MauiFilePickerSample/MainPage.xaml.cs
+++ MauiFilePickerSample/MainPage.xaml.cs
@@ -2,23 +2,49 @@
 
 public partial class MainPage : ContentPage
 {
-	int count = 0;
-
 	public MainPage()
 	{
 		InitializeComponent();
 	}
 
-	private void OnCounterClicked(object sender, EventArgs e)
+	private async void OnCounterClicked(object sender, EventArgs e)
 	{
-		count++;
+		var result = await FilePicker.PickAsync(new PickOptions
+		{
+			PickerTitle = "Pick Image Please",
+			FileTypes = FilePickerFileType.Images
+		});
 
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-			CounterBtn.Text = $"Clicked {count} times";
+		if (result == null)
+			return;
 
-		SemanticScreenReader.Announce(CounterBtn.Text);
+		var stream = await result.OpenReadAsync();
+
+		myImage.Source = ImageSource.FromStream(() => stream);
 	}
+
+	private async void PickImages_Clicked(object sender, EventArgs e)
+	{
+		// For custom file types            
+		//var customFileType =
+		//	new FilePickerFileType(new Dictionary<DevicePlatform, IEnumerable<string>>
+		//	{
+		//		 { DevicePlatform.iOS, new[] { "com.adobe.pdf" } }, // or general UTType values
+		//       { DevicePlatform.Android, new[] { "application/pdf" } },
+		//		 { DevicePlatform.WinUI, new[] { ".pdf" } },
+		//		 { DevicePlatform.Tizen, new[] { "*/*" } },
+		//		 { DevicePlatform.macOS, new[] { "pdf"} }, // or general UTType values
+		//	});
+
+		var results = await FilePicker.PickMultipleAsync(new PickOptions
+		{
+			//FileTypes = customFileType
+        });
+
+		foreach (var result in results)
+		{
+			await DisplayAlert("You picked...", result.FileName, "OK");
+		}
+    }
 }
 
